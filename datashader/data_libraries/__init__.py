607abc534208383897345ec9dbf1f2893fcab971
Jon Mease <jon.mease@gmail.com>
2019-10-03 09:03:32 -0400

    Move transfer functions to package with separate cpu/cuda utilities add cudf data library module

--- datashader/data_libraries/__init__.py
+++ datashader/data_libraries/__init__.py
@@ -5,3 +5,10 @@ try:
     from . import dask    # noqa (API import)
 except ImportError:
     pass
+
+try:
+    import cudf as _cudf  # noqa (Test cudf installed)
+    import cupy as _cupy  # noqa (Test cupy installed)
+    from . import cudf    # noqa (API import)
+except ImportError:
+    pass
